dc161d0319b7db62b5878536ce4be46f51a57bf8
XiangZhou <723395286@qq.com>
2016-05-27 15:31:40 +0800

    Update NuoMi.java

--- NuoMi.java
+++ NuoMi.java
@@ -16,6 +16,8 @@ import com.svail.util.FileTool;
 import com.svail.util.HTMLTool;
 import com.svail.util.Tool;
 
+import net.sf.json.JSONObject;
+
 public class NuoMi {
 	public static void main(String[] args) throws IOException {
 		//getRegionLink("D:/重庆基础数据抓取/基础数据/糯米网/");
@@ -28,6 +30,100 @@ public class NuoMi {
 	public static String[] CATEGORY={"1000002","962","364","380","393","880","879","690","460","881","954","878","692","392","439","391",
 			                         "884","501","389","388","883","877","488","655", "691","390","653","424","451","694","695","652","504",
 			                          "450","887","654","509","885","454","696","876","889","886","888","693","874","697","327","882","890"};
+	public static void getNuoMiContent(String link,String folder){
+		try {
+			String content = Tool.fetchURL(link);
+			content = HTMLTool.fetchURL(link, "utf-8", "get");
+			System.out.println(content);
+			FileTool.Dump(content, folder + "content.txt", "utf-8");
+			Parser parser = new Parser();
+			
+			JSONObject obj=new JSONObject();
+			if (content == null) {
+				FileTool.Dump(link, folder + "-Null.txt", "utf-8");
+			} else {
+				parser.setInputHTML(content);
+				parser.setEncoding("utf-8");
+			    HasParentFilter parentFilter1 = new HasParentFilter(new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "p-item-info")));
+			    HasParentFilter parentFilter2 = new HasParentFilter(new AndFilter(new TagNameFilter("div"),new AndFilter(parentFilter1,new HasAttributeFilter("class", "w-item-info clearfix"))));
+			    //HasParentFilter parentFilter3 = new HasParentFilter(new AndFilter(new TagNameFilter("li"),parentFilter2));
+				NodeFilter filter = new AndFilter(new TagNameFilter("h2"), parentFilter2);
+				NodeList nodes = parser.extractAllNodesThatMatch(filter);
+				if (nodes.size() != 0) {
+					for (int n = 0; n < nodes.size(); n++) {
+						TagNode no = (TagNode) nodes.elementAt(n);
+						String str=no.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
+						System.out.println(str);
+						obj.put("title", str);
+					}
+				}
+				
+				parser.reset();
+				parentFilter1 = new HasParentFilter(new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "market-price-area")));
+				filter = new AndFilter(new TagNameFilter("div"), new AndFilter(parentFilter1,new HasAttributeFilter("class", "price")));
+				nodes = parser.extractAllNodesThatMatch(filter);
+				if (nodes.size() != 0) {
+					for (int n = 0; n < nodes.size(); n++) {
+						TagNode no = (TagNode) nodes.elementAt(n);
+						String str=no.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
+						System.out.println(str);
+						obj.put("price", str);
+					}
+				}
+				
+				parser.reset();
+				parentFilter1 = new HasParentFilter(new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "current-price-area discount")));
+				filter = new AndFilter(new TagNameFilter("div"), new AndFilter(parentFilter1,new HasAttributeFilter("class", "price")));
+				nodes = parser.extractAllNodesThatMatch(filter);
+				if (nodes.size() != 0) {
+					for (int n = 0; n < nodes.size(); n++) {
+						TagNode no = (TagNode) nodes.elementAt(n);
+						String str=no.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
+						System.out.println(str);
+						obj.put("discount", str);
+					}
+				}
+				
+				parser.reset();
+				parentFilter1 = new HasParentFilter(new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "validdate-buycount-area static-hook-real static-hook-id-11")));
+				filter = new AndFilter(new TagNameFilter("div"), new AndFilter(parentFilter1,new HasAttributeFilter("class", "item-countdown-row clearfix")));
+				nodes = parser.extractAllNodesThatMatch(filter);
+				if (nodes.size() != 0) {
+					for (int n = 0; n < nodes.size(); n++) {
+						TagNode no = (TagNode) nodes.elementAt(n);
+						String str=no.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
+						System.out.println(str);
+						obj.put("valid", str);
+					}
+				}
+				
+				parser.reset();
+				//parentFilter1 = new HasParentFilter(new AndFilter(new TagNameFilter("article"),new HasAttributeFilter("mon", "merchantId=40893747")));
+				//parentFilter2 = new HasParentFilter(new AndFilter(new TagNameFilter("section"),new AndFilter(parentFilter1,new HasAttributeFilter("class", "mct-head"))));
+				filter = new AndFilter(new TagNameFilter("a"),new HasAttributeFilter("class", "mct-addr "));
+				nodes = parser.extractAllNodesThatMatch(filter);
+				if (nodes.size() != 0) {
+					for (int n = 0; n < nodes.size(); n++) {
+						TagNode no = (TagNode) nodes.elementAt(n);
+						String str=no.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
+						System.out.println(str);
+						obj.put("valid", str);
+					}
+				}
+			}
+			
+		}catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+			FileTool.Dump(link, folder + "NullLink.txt", "utf-8");
+		}
+	}
+
+	/**
+	 * 按区县和类别获取餐馆链接
+	 */
 	public static void getAllLinks(){
 		
 		
@@ -38,34 +134,35 @@ public class NuoMi {
 		String end="?#j-sort-bar";
 		
 		
-		
-		for(int i=0;i<CATEGORY.length;i++){
+		for(int i=CATEGORY.length-1;i<CATEGORY.length;i++){
 			category=CATEGORY[i];
-			for(int j=7;j<REGIONS.length;j++){
+			for(int j=REGIONS.length-7;j<REGIONS.length;j++){
 				region=REGIONS[j];
 				//https://cq.nuomi.com/1000002/3402
 				String url=start+category+"/"+region+temp+"1"+end;
 				
-				int pages=Integer.parseInt(getPages(url));
-				
-				String filename=CATEGORY[i]+"-"+REGIONS[j];
-				for(int k=0;k<pages;k++){
-					System.out.println(k);
-					url=start+category+"/"+region+temp+i+end;
-					getRestaurantLink(url,filename);
+				int pages=getPages(url);
+				if(pages>0){
+					String filename=CATEGORY[i]+"-"+REGIONS[j];
+					for(int k=0;k<pages;k++){
+						
+						url=start+category+"/"+region+temp+k+end;
+						getRestaurantLink(url,filename);
+						System.out.println(category+"类"+region+"区"+"第"+k+"页的链接获取完毕！");//890类6756区第0页的链接获取完毕！
+					}
 				}
 			}
 		}
 		
 	
 	}
-	public static String getPages(String url){
-		String pages="";
+	public static int getPages(String url){
+		int pages=0;
 		try {
 			String content = Tool.fetchURL(url);
 			//String content = HTMLTool.fetchURL(url, "utf-8", "get");
 			Parser parser = new Parser();
-			if (content == null) {
+			if (content.length() == 0) {
 				System.out.println("内容为null");
 			} else {
 				String tur ="";			
@@ -79,8 +176,8 @@ public class NuoMi {
 						TagNode no1 = (TagNode) nodes1.elementAt(n);
 						String str=no1.toPlainTextString().replace(" ", "").replace("\r\n", "").replace("\t", "").replace("\n", "");
 						if(str.indexOf("/")!=-1){
-							pages=str.substring(str.indexOf("/")+"/".length());
-							System.out.println(pages);
+							pages=Integer.parseInt(str.substring(str.indexOf("/")+"/".length()));
+							//System.out.println(pages);
 						}												
 					}
 				}							
@@ -94,14 +191,19 @@ public class NuoMi {
 		return pages;
 	}
 	
-	
+	/**
+	 * 获取每个餐馆的链接
+	 * @param url
+	 * @param folder
+	 */
 	public static void getRestaurantLink(String url,String folder){
 		
 		try {
 			String content = Tool.fetchURL(url);
 			//content = HTMLTool.fetchURL(url, "utf-8", "get");
 			Parser parser = new Parser();
-			if (content == null) {
+			if (content.length()==0) {
+				System.out.println("该链接不能获取："+url);
 				FileTool.Dump(url, folder + "-Null.txt", "utf-8");
 			} else {
 				String tur ="";
@@ -115,7 +217,7 @@ public class NuoMi {
 					for (int n = 0; n < nodes1.size(); n++) {
 						TagNode no1 = (TagNode) nodes1.elementAt(n);
 						tur = no1.getAttribute("href");
-						System.out.println(tur);
+						//System.out.println(tur);
 						FileTool.Dump(tur, "D:/重庆基础数据抓取/基础数据/糯米网/餐馆分类链接/"+folder+".txt", "utf-8");
 					}
 				}							
